05c0f5d5f3afbc7d01ab4568289f9d02f8bb0717
aakaashpatel <akashpatel8818@gmail.com>
2023-08-07 17:13:43 +0530

    fist layout completed

--- script.js
+++ script.js
@@ -33,3 +33,39 @@ ScrollTrigger.refresh();
 }
 
 loco();
+
+
+gsap.to("#page>video",{
+  scrollTrigger:{
+      trigger:`#page>video`,
+      start:`2% top`,
+      end:`bottom top`,
+      // markers:true,
+      scroller:`#main`
+  },
+    onStart:()=>{
+      document.querySelector('#page>video').play()
+    }
+})
+
+
+gsap.to('#page',{
+  scrollTrigger:{
+    trigger:`#page`,
+    start:`top top`,
+    end:`bottom top`,
+    scroller:`#main`,
+    pin:true
+  }
+})
+
+
+gsap.to('#page=bottom',{
+  scrollTrigger:{
+    trigger:`#page-bottom`,
+    start:`5% top`,
+    end:`bottom top`,
+    scroller:`#main`,
+  },
+    display:none
+})
